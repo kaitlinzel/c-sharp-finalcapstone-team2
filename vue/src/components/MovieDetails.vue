0303ab06cc7030f1cc2b8538d7270e818ad8ccce
Darya Taraban <daryastaraban@gmail.com>
2023-04-10 16:15:06 -0400

    added movie card and movie detail component& views

---
+++ vue/src/components/MovieDetails.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <h2>
+        {{ movie.title }}
+    </h2>    
+    <!-- <h2 class="book-author">{{ book.author }}</h2>
+    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
+    <p>I have {{ book.read ? '' : "not" }} read this book</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
