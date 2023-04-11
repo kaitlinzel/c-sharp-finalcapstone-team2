fcb2e708f976364236595fc0ac8cee60fcf75027
Christaline Ivey <iveychristaline@gmail.com>
2023-04-11 19:30:09 -0400

    clearing my branch

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <h1>Welcome user{{this.user}}<!-- want to be able to input the logged in users name in here-->
-      <img class="user_icon" src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"/></h1>
+    <h1>
+      Welcome User 
+      <!-- want to be able to input the logged in users name in here-->
+      <img 
+        class="user_icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+      />
+    </h1>
     <div class="nav">
       <h2 class="for_you">Movies For You</h2>
       <h2 class="cont">Continue Watching</h2>
@@ -10,45 +16,51 @@
     <p class="movie_list">These are a few of our movies.</p>
     <div class="container">
       <!-- <movie-card/> -->
-      <movies-list/>
+      <movies-list />
     </div>
   </div>
 </template>
 
 <script>
 //import MovieCard from '../components/MovieCard.vue';
-import MoviesList from '../components/MoviesList.vue';
+import MoviesList from "../components/MoviesList.vue";
 export default {
-  components: { //MovieCard,
-   MoviesList },
-  name: "home"
+  components: {
+    //MovieCard,
+    MoviesList,
+  },
+  name: "home",
 };
 </script>
 
 <style scoped>
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
-html, body, div, img {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font-family: fantasy;
-    vertical-align: baseline;
+html,
+body,
+div,
+img {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font-family: fantasy;
+  vertical-align: baseline;
 }
 body {
-   line-height: 1;
+  line-height: 1;
 }
 h1 {
-   display: flex;
-   justify-content: right;
+  display: flex;
+  justify-content: right;
 }
 .nav > h2 {
   width: 350px;
   padding: 5px;
   display: flex;
   justify-content: space-around;
+  align-items: center;
 }
 p.movie_list {
   display: flex;
@@ -60,10 +72,10 @@ img.user_icon {
   width: 50px;
   height: 50px;
   background: rgb(12, 79, 223);
-  border-radius: 50%;
+  border-radius: 60%;
   padding: 5px;
 }
-.container { 
+.container {
   width: 350px;
   margin: 5px;
   padding: 5px;
