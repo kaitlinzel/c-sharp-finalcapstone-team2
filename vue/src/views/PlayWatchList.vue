3531b2db642a7ff911c646d0bca6e0d2772f323a
Jacob Natowicz <jnatowicz@gmail.com>
2023-04-12 11:24:49 -0400

    Small changes on playlist

--- vue/src/views/PlayWatchList.vue
+++ vue/src/views/PlayWatchList.vue
@@ -1,17 +1,33 @@
 <template>
   <div class= watchlist> 
       <h1>Welcome!</h1>
-      <h2>Here you can create a watchlist or a playlist.</h2>   
+      <h2>Here you can create a watchlist or a playlist.</h2> 
+
+      <div class= setPlaylist>
+        <p>What would you like to call your playlist?</p>
+      <input v-model="inputPlaylistName" type="text" placeholder="Playlist Name">
+      </div>  
+
+      
+  
   </div>
 </template>
 
 <script>
 
 export default {
-
+name: 'playwatchlist',
+data() {
+  return {
+    inputPlaylistName: ""
+  }
+}
 }
 </script>
 
 <style>
-
+.setPlaylist {
+  display: flex;
+  
+}
 </style>
